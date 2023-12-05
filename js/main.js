b3a60fed6bf48955ea1c0094a15fcad2e751ea75
Bhuvnish <b_arora163217@fanshaweonline.ca>
2023-12-05 13:04:37 -0500

    fixing things

--- js/main.js
+++ js/main.js
@@ -5,6 +5,12 @@
 //gsap.set(".st0",{visibility:"visible"});
 
 //gsap.from(".st0",2,{drawsvg:0,ease : "power1.inout"})//
+const gallery = document.getElementById('gallery');
+
+gallery.addEventListener('wheel', (event) => {
+  gallery.scrollLeft += event.deltaY;
+  event.preventDefault();
+});
 
 })();
 
