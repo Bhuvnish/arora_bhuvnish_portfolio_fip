2ee0f22d1c1201412512750fecfb671436afaf06
Bhuvnish <b_arora163217@fanshaweonline.ca>
2023-12-05 19:34:41 -0500

    javascripy

--- js/main.js
+++ js/main.js
@@ -5,11 +5,28 @@
 //gsap.set(".st0",{visibility:"visible"});
 
 //gsap.from(".st0",2,{drawsvg:0,ease : "power1.inout"})//
-const gallery = document.getElementById('gallery');
+//const gallery = document.getElementById('gallery');
 
-gallery.addEventListener('wheel', (event) => {
-  gallery.scrollLeft += event.deltaY;
-  event.preventDefault();
+//gallery.addEventListener('wheel', (event) => {
+ // gallery.scrollLeft += event.deltaY;
+ // event.preventDefault();
+//});//
+
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+          if (entry.isIntersecting) {
+              entry.target.classList.add('in-view');
+          }
+      });
+  });
+
+  document.querySelectorAll('.grid-item').forEach(item => {
+      observer.observe(item);
+  });
 });
 
 })();
